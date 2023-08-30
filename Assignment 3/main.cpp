930f19ef0a9f48c67769a9d030c89073818f75db
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-08-30 13:22:55 -0500

    Initial Commit

---
+++ Assignment 3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment 3
+//
+//  Created by scott sheble on 8/30/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
