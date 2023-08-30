d77db16fec9ce963fc8bbf6437e4e208f36a099a
scott sheble <117122862+scottsheble@users.noreply.github.com>
2023-08-30 15:10:03 -0500

    Add logic for tax brackets

--- Assignment 3/main.cpp
+++ Assignment 3/main.cpp
@@ -17,8 +17,25 @@
 
 using namespace std;
 
+double grossIncome = 0;
+double incomeTax = 0;
+
 int main()
 {
-    std::cout << "What is your gross income?";
+    std::cout << "What is your gross income? ";
+    std::cin >> grossIncome;
+    if (grossIncome <= 11000)
+        incomeTax = (0.1 * 11000);
+    else if (grossIncome > 11000 && grossIncome <= 44725)
+        incomeTax = (0.1 * 11000) + (0.12 * 33725);
+    else if (grossIncome > 44725 && grossIncome <= 95375)
+        incomeTax = (0.1 * 11000) + (0.12 * 33725) + (0.22 * 50650);
+    else if (grossIncome > 95375)
+        incomeTax = (0.1 * 11000) + (0.12 * 33725) + (0.22 * 50650) + (0.24 * 4625);
+        
+    std::cout << std::endl;
+    std::cout << "Gross Income: " << fixed << setprecision(2) <<  grossIncome << std::endl;
+    std::cout << "Income Tax " << fixed << setprecision(2) << incomeTax << std::endl;
+    std::cout << std::endl;
     return 0;
 }
